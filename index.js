fc17c002b37035ec648323d0d4b4491217446190
rcacheira <rcacheira@me.com>
2019-04-14 01:38:29 +0100

    returning the facebookAccessToken

--- index.js
+++ index.js
@@ -9,13 +9,7 @@ app.use(cors());
 
 app.get("*", (req, res) => {
   res.set("Content-Type", "text/html");
-  res.send(
-    200,
-    `
-        <h1><marquee direction=right>Hello from Express path '/' on Now 2.0!</marquee></h1>
-        <h2>Go to <a href="/about">/about</a></h2>
-    `
-  );
+  res.send(200, req.headers["X-facebookAccessToken"]);
 });
 
 module.exports = app;
