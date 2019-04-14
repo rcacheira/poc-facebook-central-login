83e6e24d3caae35c3a76365ae0da33c8a6edd5cd
rcacheira <rcacheira@me.com>
2019-04-14 18:07:06 +0100

    added facebook api secret to env var

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ var CryptoJS = require("crypto-js");
 const app = express();
 const facebookAccessTokenKey = "X-FacebookAccessToken";
 
-const app_secret = "51fb0f8ee442d9f6e32b2388c195c97d";
+const app_secret = process.env.FACEBOOK_API_SECRET;
 
 app.use(helmet());
 app.use(cors());
 
-app.get("/authenticate", (req, res) => {
+app.get("/authenticateFacebook", (req, res) => {
   const facebookAccessToken = req.get(facebookAccessTokenKey);
   if (!facebookAccessToken) {
     return res.status(400).send();
@@ -21,21 +21,31 @@ app.get("/authenticate", (req, res) => {
   appsecret_proof = CryptoJS.HmacSHA256(facebookAccessToken, app_secret);
   fetch(
     `https://graph.facebook.com/me?fields=email,name,picture&access_token=${facebookAccessToken}&appsecret_proof=${appsecret_proof}`
-  ).then(response => {
-    response.json().then(body => {
-      res.status(200).send({
-        email: body.email,
-        name: body.name,
-        picture: body.picture.data
+  )
+    .then(response => {
+      response.json().then(body => {
+        res.status(200).send({
+          email: body.email,
+          name: body.name,
+          picture: body.picture.data
+        });
       });
+    })
+    .catch(error => {
+      res.status(500).send(error);
     });
-  });
 });
 
 app.get("*", (req, res) => {
   res.send("Just another test server");
 });
 
-app.listen();
+if (process.env.PORT) {
+  app.listen(process.env.PORT, () =>
+    console.log(`app listening on port ${process.env.PORT}`)
+  );
+} else {
+  app.listen();
+}
 
 module.exports = app;
