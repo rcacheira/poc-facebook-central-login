1bef92580e6f08aabbd2365a7c02c060433246f0
rcacheira <rcacheira@me.com>
2019-04-14 01:21:25 +0100

    minor changes

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-const express = require('express')
-const helmet = require('helmet')
+const express = require("express");
+const helmet = require("helmet");
 
-const app = express()
+const app = express();
 
-// add some security-related headers to the response
-app.use(helmet())
+app.use(helmet());
 
-app.get('*', (req, res) => {
-    res.set('Content-Type', 'text/html')
-    res.send(200, `
+app.get("*", (req, res) => {
+  res.set("Content-Type", "text/html");
+  res.send(
+    200,
+    `
         <h1><marquee direction=right>Hello from Express path '/' on Now 2.0!</marquee></h1>
         <h2>Go to <a href="/about">/about</a></h2>
-    `)
-})
+    `
+  );
+});
 
-module.exports = app
+module.exports = app;
