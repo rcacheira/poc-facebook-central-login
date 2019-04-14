4439d724eb88fa818e01014a72655214ba111c10
rcacheira <rcacheira@me.com>
2019-04-14 01:33:25 +0100

    added cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const helmet = require("helmet");
+const cors = require("cors");
 
 const app = express();
 
 app.use(helmet());
+app.use(cors());
 
 app.get("*", (req, res) => {
   res.set("Content-Type", "text/html");
